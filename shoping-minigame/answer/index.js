f70e57e1d6e695d70a1024cb57f278fcf151dce3
unknown <wowns9270@gmail.com>
2021-02-09 18:25:13 +0900

    mini-innerHtml-upgrade

--- shoping-minigame/answer/index.js
+++ shoping-minigame/answer/index.js
@@ -1,4 +1,4 @@
-
+'ues stric';
 const tshirt_btn = document.querySelector('.btn.tshirt');
 const pants_btn = document.querySelector('.btn.pants');
 const skirt_btn = document.querySelector('.btn.skirt');
@@ -19,41 +19,85 @@ const a = async() =>{
     }
 }
 
+
 function displayItems(items){
     const container = document.querySelector('.items');
     container.innerHTML= items.map((item)=>{
         return (
-            `<li class="item">
+            `<li class="item " data-type ='${item.type}' data-color ='${item.color}'>
             <img src=".${item.image}" alt="${item.type}" class="item_thumbnail">
             <span class="item_description">${item.gender}, ${item.size}</span>
             </li>
             `
         )
     }).join('');
+}
+
+function setEventListeners(items){
+    const logo = document.querySelector('.logo');
+
+    const buttons = document.querySelector('.btns');
+
+     logo.addEventListener('click', ()=>{
+         displayItems(items)
+     });
+
+    buttons.addEventListener('click', (event)=> onButtonClick(event));
 
 }
+
+function onButtonClick(event){
+   const dataset = event.target.dataset;
+
+   const key = dataset.key;
+   const value = dataset.value;
+
+   if(key == null || value == null) return ;
+
+
+   updateItems(key , value);
+}
+
+function updateItems( key , value){
+
+    const items = document.querySelectorAll('.item');
+
+    items.forEach( item => {
+        if(item.dataset[key] === value){
+            item.classList.remove('invisible');
+        }
+        else{
+            item.classList.add('invisible');
+        }
+
+    });
+
+}
+
+
 
 a()
 .then( (res) =>{
     //console.log(res);
-    displayItems(res);
-    tshirt_btn.addEventListener('click', () =>{
-        displayItems(res.filter( (item) => item.type === 'tshirt'))
-    })
-    pants_btn.addEventListener('click', () =>{
-        displayItems(res.filter( (item) => item.type === 'pants'))
-    })
-    skirt_btn.addEventListener('click', () =>{
-        displayItems(res.filter( (item) => item.type === 'skirt'))
-    })
-    blue_btn.addEventListener('click', () =>{
-        displayItems(res.filter( (item) => item.color === 'blue'))
-    })
-    yellow_btn.addEventListener('click', () =>{
-        displayItems(res.filter( (item) => item.color === 'yellow'))
-    })
-    pink_btn.addEventListener('click', () =>{
-        displayItems(res.filter( (item) => item.color === 'pink'))
-    })
+    displayItems(res);  
+    setEventListeners(res);
+    // tshirt_btn.addEventListener('click', () =>{
+    //     displayItems(res.filter( (item) => item.type === 'tshirt'))
+    // })
+    // pants_btn.addEventListener('click', () =>{
+    //     displayItems(res.filter( (item) => item.type === 'pants'))
+    // })
+    // skirt_btn.addEventListener('click', () =>{
+    //     displayItems(res.filter( (item) => item.type === 'skirt'))
+    // })
+    // blue_btn.addEventListener('click', () =>{
+    //     displayItems(res.filter( (item) => item.color === 'blue'))
+    // })
+    // yellow_btn.addEventListener('click', () =>{
+    //     displayItems(res.filter( (item) => item.color === 'yellow'))
+    // })
+    // pink_btn.addEventListener('click', () =>{
+    //     displayItems(res.filter( (item) => item.color === 'pink'))
+    // })
 
 })
